2b41daaae759308abd4124e6c351c1fc8c943160
Enrique <ecvoracle@gmail.com>
2021-07-27 12:37:38 +0100

    Added journalList functions modifiers

--- contracts/journalList.sol
+++ contracts/journalList.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+pragma experimental ABIEncoderV2;
 
 contract journalList {
     uint256 private entryCounter = 0;
@@ -25,12 +26,32 @@ contract journalList {
 
     event entryUpdated(uint256 id, string title, string content, uint256 score);
 
+    // modifier functions that is executed before rest of code
+    // it validates that fields (title & content) are not empty
+    modifier emptyString(string memory _title, string memory _content) {
+        require(bytes(_title).length > 0, "Not valid title. Must contain one.");
+        require(
+            bytes(_content).length > 0,
+            "Not valid content. Must contain one."
+        );
+        _;
+    }
+
+    // modifier functions that is executed before rest of code
+    // it validates that score is between 1 to 10 (both included)
+    modifier validScore(uint256 _score) {
+        require(
+            _score >= 0 && _score <= 10,
+            "Not valid score. Only allow: 1-10."
+        );
+        _;
+    }
+
     function newEntry(
-        uint256 _date,
         string memory _title,
         string memory _content,
         uint256 _score
-    ) public {
+    ) public validScore(_score) emptyString(_title, _content) {
         journalEntries[entryCounter] = journalEntry(
             entryCounter,
             block.timestamp,
@@ -40,7 +61,13 @@ contract journalList {
             msg.sender
         );
         entryCounter++;
-        emit entryCreated(entryCounter, _date, _title, _content, _score);
+        emit entryCreated(
+            entryCounter,
+            block.timestamp,
+            _title,
+            _content,
+            _score
+        );
     }
 
     function showListEntries() public view returns (journalEntry[] memory) {
