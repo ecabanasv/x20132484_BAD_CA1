d067e3e10bf0f6d1cc7276c9b2db5ed2ab35912a
Enrique <ecvoracle@gmail.com>
2021-07-30 20:08:32 +0100

    Fixed JWT login system

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,27 @@ var jwt = require('jsonwebtoken');
 
 // Home - router
 router.get("/", function (req, res, next) {
-  res.render("index", { page: "Home", menuID: "home" });
+  var token = req.cookies.token;
+  var userName = null;
+  try {
+  userName = jwt.verify(token, "JournalJWT").username;
+	res.render("index", { page: "Home", menuID: "home", name: userName });
+
+  } catch (error) {
+    return res.render("index", { page: "Home", menuID: "home", name: null});
+  }
 });
 
 // Ad new entry - router
 router.get("/new-entry", async function (req, res, next) {
   var token = req.cookies.token;
-  var username_id = null;
-  // var metaAddress = null;
+  var userName = null;
   try {
-    username_id = jwt.verify(token, "JournalJWT")._id;
-    // metaAddress = jwt.verify(token, "JournalJWT").address;
-    console.log(username_id);
-    // console.log(metaAddress);
+    userName = jwt.verify(token, "JournalJWT").username;
+    res.render("new-entry", { page: "New Entry", menuID: "new-entry", name: userName  });
   } catch (error) {
-    console.log(error);
-    return res.redirect("/logout");
+    return res.render("index", { page: "Home", menuID: "home", name: null});
   }
-
-  res.render("new-entry", { page: "New Entry", menuID: "new-entry" });
 });
 
 router.post("/new-entry", function (req, res, next) {});
@@ -31,30 +33,41 @@ router.post("/new-entry", function (req, res, next) {});
 // List of entries - router
 router.get("/list-entries", function (req, res, next) {
   var token = req.cookies.token;
-  var username_id = null;
+  var userName = null;
   try {
-    username_id = jwt.verify(token, "JournalJWT");
+    userName = jwt.verify(token, "JournalJWT").username;
+    res.render("list-entries", { page: "List of entries", menuID: "list-entries", name: userName  });
   } catch (error) {
-    console.log(error);
-    return res.redirect("/logout");
+    return res.render("index", { page: "Home", menuID: "home", name: null});
   }
-
-  res.render("list-entries", {
-    page: "List of entries",
-    menuID: "list-entries",
-  });
 });
 
 router.post("/list-entries", function (req, res, next) {});
 
 // FAQ - router
 router.get("/faq", function (req, res, next) {
-  res.render("faq", { page: "FAQ", menuID: "faq" });
+  var token = req.cookies.token;
+  var userName = null;
+  // var metaAddress = null;
+  try {
+  userName = jwt.verify(req.cookies.token, "JournalJWT").username;
+	res.render("faq", { page: "FAQ", menuID: "faq", name: userName});
+
+  } catch (error) {
+    res.render("faq", { page: "FAQ", menuID: "faq", name: null});
+  }
 });
 
 // Contact - router
 router.get("/contact", function (req, res, next) {
-  res.render("contact", { page: "Contact", menuID: "contact" });
+  var token = req.cookies.token;
+  var userName = null;
+  try {
+    userName = jwt.verify(token, "JournalJWT").username;
+    res.render("contact", { page: "Contact", menuID: "contact", name: userName  });
+  } catch (error) {
+    return res.render("contact", { page: "Contact", menuID: "contact", name: null});
+  }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,14 @@ var user_controller = require("../controllers/usersController");
 // Login router
 
 router.get("/login", async function (req, res, next) {
-  res.render("login", { page: "Login", menuID: "login" });
+  var token = req.cookies.token;
+  var userName = null;
+  try {
+    userName = jwt.verify(token, "JournalJWT").username;
+    res.render("home", { page: "Home", menuID: "home", name: userName  });
+  } catch (error) {
+    return res.render("login", { page: "Login", menuID: "login", name: null});
+  }
 });
 
 router.post("/login", user_controller.user_login_post);
@@ -15,7 +22,14 @@ router.post("/login", user_controller.user_login_post);
 // Signup router
 
 router.get("/signup", async function (req, res, next) {
-  res.render("signup", { page: "Signup", menuID: "signup" });
+  var token = req.cookies.token;
+  var userName = null;
+  try {
+    userName = jwt.verify(token, "JournalJWT").username;
+    res.render("home", { page: "Home", menuID: "home", name: userName  });
+  } catch (error) {
+    return res.render("signup", { page: "Signup", menuID: "signup", name: null});
+  }
 });
 
 router.post("/signup", user_controller.user_signup_post);
